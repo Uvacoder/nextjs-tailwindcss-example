809095960a3138ff10fd89bd25272ab6184354c8
Kevin Wolf <hi@kevinwolf.dev>
2020-10-09 09:34:43 -0600

    Style Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
-    <main>
-      <p>Next.js + TailwindCSS Demo</p>
+    <main className="min-h-screen flex items-center justify-center">
+      <p className="shadow-lg rounded-lg bg-white p-12 text-4xl font-bold">
+        Next.js + TailwindCSS Demo
+      </p>
     </main>
   )
 }
